06b1168cbb606f8e42697bbe2607b0ce89ab866d
Shirux <shirux123@gmail.com>
2020-02-09 21:53:32 -0500

    first push

--- js/script.js
+++ js/script.js
@@ -3,48 +3,83 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+ Global variables to work this project
+ studentList: All the elements LI on the student List UL
+ maxPerPage:  constant that defines how many students will be displayed per page
 ***/
-
-
-
+let studentList = document.querySelectorAll('ul.student-list li');
+const maxPerPage = 10;
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+   Function that will display the elements from the page you 
+   pass as a parameter. 
+   List: The list containing the elements you want to hide and display on your pagination
+   Page: The actual page
 ***/
+/**
+ * 
+ * @param {*} list 
+ * @param {*} page 
+ */
+const showPage = (list, page) => {
+   const startIndex = (page * maxPerPage) - maxPerPage;
+   const endIndex = (page * maxPerPage);
 
+   for (let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         list[i].style.display = "block";
+      } else {
+         list[i].style.display = "none";
+      }
+   }
+}
 
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+/**
+ * Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
-***/
+ * @param {*} list 
+ */
+const appendPageLinks = (list) => {
+   //create div where the pagination buttons are located
+   let paginationDiv = document.createElement('div');
+   paginationDiv.classList.add('pagination');
+
+   //create every button for every page inside an UL
+   let ul = document.createElement('ul');
+   let amountOfPages = Math.ceil(list.length/maxPerPage);
+   
+   appendList(ul, list);
+   for(let i = 1; i <= amountOfPages; i++){
+      let li = document.createElement('li');
+      let a = document.createElement('a');
+      a.setAttribute('href', '#');
+      a.textContent = i;
+      li.appendChild(a);
+      ul.appendChild(li);
+   }
+   let firstPage = ul.firstElementChild;
+   firstPage.querySelector('a').classList.add('active');
+   paginationDiv.appendChild(ul);
+
+   let pageDiv = document.querySelector('.page');
+   pageDiv.appendChild(paginationDiv);
+}
+
+/**
+ * 
+ * @param {*} ul 
+ * @param {*} list 
+ */
+const appendList = (ul, list) => {
+
+}
 
 
-
+appendPageLinks(studentList);
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
